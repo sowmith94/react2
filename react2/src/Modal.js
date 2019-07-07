80b5259cfce46ab02332137a7b5996bd18065314
Sowmith <smandad2@asu.edu>
2019-07-07 14:55:06 -0700

    added styling using styled components to modal component

--- react2/src/Modal.js
+++ react2/src/Modal.js
@@ -9,8 +9,11 @@ export default class Modal extends Component {
       <Portal>
         {on && (
           <ModalWrapper>
-            <button onClick={toggle}>Close Modal</button>
-            {children}
+            <ModalCard>
+              <CloseButton onClick={toggle}>Close Modal</CloseButton>
+              {children}
+            </ModalCard>
+            <Background onClick={toggle} />
           </ModalWrapper>
         )}
       </Portal>
@@ -24,5 +27,33 @@ const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: yellow;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  z-index: 10;
+  background: white;
+  min-width: 400px;
+  padding: 15px;
+  border-radius: 7px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const Background = styled.div`
+  background: black;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 100%;
+  opacity: 0.5;
 `;
